5a0bdf455232de6ebb6a71e0f1a8d621a89a91f2
Ryan Grove <ryan@wonko.com>
2010-09-07 20:06:47 -0700

    Add lots and lots of stopwords.

--- conf/stopwords.rb
+++ conf/stopwords.rb
@@ -2,6 +2,8 @@ module YUITweets; module Config
 
 # These words will be ignored by the Bayesian classifier.
 STOPWORDS = Set.new(%w[
+  #yui
+  0
   1
   2
   3
@@ -11,18 +13,23 @@ STOPWORDS = Set.new(%w[
   7
   8
   9
-  0
-  #yui
   a
+  a's
+  able
   about
   above
+  according
+  accordingly
   across
+  actually
   after
   afterwards
   again
   against
   ain't
   all
+  allow
+  allows
   almost
   alone
   along
@@ -39,17 +46,31 @@ STOPWORDS = Set.new(%w[
   and
   another
   any
+  anybody
   anyhow
   anyone
   anything
   anyway
+  anyways
   anywhere
+  apart
+  appear
+  appreciate
+  appropriate
   are
   aren't
   around
   as
+  aside
+  ask
+  asking
+  associated
   at
+  available
+  away
+  awfully
   back
+  bad
   be
   became
   because
@@ -61,110 +82,222 @@ STOPWORDS = Set.new(%w[
   beforehand
   behind
   being
+  believe
   below
   beside
   besides
+  best
+  better
   between
   beyond
   both
   bottom
+  brief
   but
   by
+  c'mon
+  c's
+  came
   can
-  cannot
   can't
+  cannot
+  cant
+  cause
+  causes
+  certain
+  certainly
+  changes
+  clearly
   co
+  com
+  come
+  comes
   computer
   con
+  concerning
+  consequently
+  consider
+  considering
+  contain
+  containing
+  contains
+  corresponding
   could
   couldn't
-  de
+  course
+  currently
+  definitely
   describe
+  described
+  despite
   detail
   did
   didn't
+  different
   do
-  done
+  does
+  doesn't
+  doing
   don't
+  done
   down
+  downwards
   due
   during
   each
-  eg
+  edu
+  eight
   either
+  else
   elsewhere
   empty
   enough
+  entirely
+  especially
+  et
   etc
   even
   ever
   every
+  everybody
   everyone
   everything
   everywhere
+  ex
+  exactly
+  example
   except
+  far
   few
+  fifth
   fill
   find
   fire
   first
+  five
+  followed
+  following
+  follows
   for
+  former
+  formerly
+  forth
   found
+  four
   from
   front
   full
   further
+  furthermore
   get
+  gets
+  getting
   give
+  given
+  gives
   go
+  goes
+  going
+  gone
+  good
+  got
+  gotten
+  greetings
   had
+  hadn't
+  happens
+  hardly
   has
   hasn't
   have
   haven't
+  having
   he
   he's
+  hello
+  help
   hence
   her
   here
+  here's
   hereafter
   hereby
   herein
   hereupon
   hers
+  herself
   hi
   him
+  himself
   his
+  hither
+  hopefully
   how
+  howbeit
   however
   i
   i'd
-  if
   i'll
   i'm
+  i've
+  if
+  ignored
+  immediate
   in
+  inasmuch
   inc
   indeed
+  indicate
+  indicated
+  indicates
+  inner
+  insofar
+  instead
   interest
   into
+  inward
   is
   isn't
   it
-  its
+  it'd
+  it'll
   it's
-  i've
+  its
+  itself
+  just
   keep
+  keeps
+  kept
+  know
+  known
+  knows
   last
+  lately
+  later
   latter
   latterly
   least
   less
+  lest
+  let
+  let's
+  like
+  liked
+  likely
+  little
+  look
+  looking
+  looks
   ltd
   made
+  mainly
   many
   may
+  maybe
   me
+  mean
   meanwhile
+  merely
   might
   mine
   more
@@ -176,80 +309,159 @@ STOPWORDS = Set.new(%w[
   must
   mustn't
   my
+  myself
   name
   namely
+  nd
+  near
+  nearly
+  necessary
+  need
+  needs
   neither
   never
   nevertheless
+  new
   next
   nine
   no
   nobody
+  non
   none
   noone
   nor
+  normally
   not
   nothing
+  novel
   now
   nowhere
+  obviously
   of
   off
   often
   oh
+  ok
+  okay
+  old
   on
+  once
   one
+  ones
   only
   onto
   or
   other
   others
   otherwise
+  ought
   our
   ours
   ourselves
   out
+  outside
   over
+  overall
   own
   part
+  particular
+  particularly
   per
   perhaps
+  placed
   please
+  plus
+  possible
+  presumably
+  probably
+  provides
   put
+  que
+  quite
+  qv
   rather
+  rd
   re
+  really
+  reasonably
+  regarding
+  regardless
+  regards
+  relatively
+  respectively
   retweet
+  right
   rt
+  said
   same
+  saw
+  say
+  saying
+  says
+  second
+  secondly
   see
+  seeing
   seem
   seemed
   seeming
   seems
+  seen
+  self
+  selves
+  sensible
+  sent
+  serious
+  seriously
+  seven
   several
+  shall
   she
   she's
   should
+  shouldn't
   show
   side
   since
   sincere
+  six
   so
   some
+  somebody
   somehow
   someone
   something
   sometime
   sometimes
+  somewhat
   somewhere
+  soon
+  sorry
+  specified
+  specify
+  specifying
   still
+  sub
   such
+  sup
+  sure
+  t's
   take
+  taken
+  tell
   ten
+  tends
+  th
   than
+  thank
+  thanks
+  thanx
   that
   that's
+  thats
   the
   their
+  theirs
   them
   themselves
   then
@@ -260,13 +472,21 @@ STOPWORDS = Set.new(%w[
   thereby
   therefore
   therein
+  theres
   thereupon
   these
   they
+  they'd
+  they'll
+  they're
+  they've
   thick
   thin
+  think
   third
   this
+  thorough
+  thoroughly
   those
   though
   three
@@ -277,24 +497,57 @@ STOPWORDS = Set.new(%w[
   to
   together
   too
+  took
   top
   toward
   towards
+  tried
+  tries
+  truly
+  try
+  trying
   tweet
   tweets
+  twice
   twitter
+  two
   un
   under
+  unfortunately
+  unless
+  unlikely
   until
+  unto
   up
   upon
   us
+  use
+  used
+  useful
+  uses
+  using
+  usually
+  value
+  various
   very
   via
+  viz
+  vs
+  want
+  wants
   was
+  wasn't
+  way
   we
+  we'd
+  we'll
+  we're
+  we've
+  welcome
   well
+  went
   were
+  weren't
   what
   what's
   whatever
@@ -302,6 +555,7 @@ STOPWORDS = Set.new(%w[
   whence
   whenever
   where
+  where's
   whereafter
   whereas
   whereby
@@ -313,28 +567,36 @@ STOPWORDS = Set.new(%w[
   while
   whither
   who
+  who's
   whoever
   whole
   whom
   whose
   why
   will
+  willing
+  wish
   with
   within
   without
   won't
+  wonder
   would
   wouldn't
   yes
   yet
   you
-  your
+  you'd
+  you'll
   you're
+  you've
+  your
   yours
   yourself
   yourselves
   yui
   yui's
+  zero
 ])
 
 end; end
